c9655cdca46ad7f5890fe45337e44be1dcd1b7b0
Isa Giongo <isagiongo@gmail.com>
2019-07-06 13:10:50 -0300

    Implementa lista de users

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,22 @@
+import { UserService } from './../user.service';
+import { User } from './../user';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users: User[];
+
+  constructor(private userService: UserService ) { }
+
+  ngOnInit() {
+    this.userService.findAll().subscribe(data => {
+      this.users = data;
+    });
+  }
+
+}
